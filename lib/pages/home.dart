dd7077d6c52f2efcff0d49b916d0be278a15eaf3
Khavazov <khavazovml@gmail.com>
2021-08-04 16:02:35 +0600

    First commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String _userToDo;
+  List todoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    todoList.addAll(['Buy milk', 'Wash dishes', 'Buy carrots']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('ToDo'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: todoList.length ,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: Key(todoList[index]),
+              child: Card(
+                child: ListTile(
+                    title: Text(todoList[index]),
+                    trailing: IconButton(
+                    icon: Icon(
+                    Icons.delete_sweep,
+                  color: Colors.deepOrangeAccent,
+                ),
+                      onPressed: (){
+                      setState(() {
+                        todoList.removeAt(index);
+                      });
+                      },
+                ),
+                ),
+              ),
+              onDismissed: (direction) {
+                setState(() {
+                todoList.removeAt(index);
+                });
+              },
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.greenAccent,
+        onPressed: (){
+          showDialog(context: context, builder: (BuildContext context){
+            return AlertDialog(
+              title: Text('Add element'),
+              content: TextField(
+                onChanged: (String value){
+                  _userToDo = value;
+
+                },
+              ),
+              actions: [
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    todoList.add(_userToDo);
+                  });
+
+                  Navigator.of(context).pop();
+                 }, child: Text('Add'))
+              ],
+            );
+          });
+        },
+        child: Icon(
+          Icons.add_box,
+          color: Colors.white
+        ),
+      ),
+    );
+  }
+}
